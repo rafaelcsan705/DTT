e10390dd73f93ffd2a5b1e1cda96c36df9b875d8
Rafael Santos <rafaelcsan705@gmail.com>
2020-01-16 15:19:19 +0000

    Create new ViewController's

---
+++ RSR Pechhulp/Controllers/InfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InfoViewController.swift
+//  RSR Pechhulp
+//
+//  Created by ES-Team on 13/01/2020.
+//  Copyright © 2020 Rafael Santos. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
